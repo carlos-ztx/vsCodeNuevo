025b6f1f7e2f0c9e57b330dbbbcf1d897394f85b
Carlos <carlosmendez344@gmail.com>
2023-08-03 17:55:03 -0400

    commit inicial

---
+++ src/main.py
@@ -0,0 +1,19 @@
+def hola(texto):
+     print(texto)
+
+def opera(n, m):
+     resultado = n + m
+     return resultado
+
+
+def operac(n, m):
+    resultado = n - m
+    return resultado
+
+
+variable_de_texto = "Hola"
+
+hola("AHHHHHHHHHHHHHHH")
+hola(variable_de_texto)
+print(opera(5,3))
+print(operac(5,3))
